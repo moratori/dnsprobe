6ba2814eeb0112b476efef4a0fcdf4f357876a66
moratori <hoko1993@yahoo.co.jp>
2019-03-02 19:14:22 +0900

    リファクタリングの実施

--- src/main_measurer.py
+++ src/main_measurer.py
@@ -5,9 +5,9 @@ docstring is here
 """
 
 import common.framework as framework
-
 import requests
 import traceback
+import namedtupled
 import sys
 import json
 import socket
@@ -15,15 +15,124 @@ import binascii
 import netifaces
 import ipaddress
 import datetime
-import influxdb
+import time
 import dns.name
 import dns.message
 import dns.query
 import dns.rdatatype
 import dns.exception
 
+from influxdb import InfluxDBClient
+from multiprocessing.pool import ThreadPool
 
-class Measurer(framework.Setup):
+
+class MeasurementDataConverter():
+
+    def __init__(self, logger, cnfg, cnfs):
+        self.record_parser = {}
+        self.logger = logger
+        self.cnfg = cnfg
+        self.cnfs = cnfs
+
+    def add_record_parser(self, qname, rtype, func):
+        self.record_parser[(qname, rtype)] = func
+
+    def parse_response_to_fields(self, qname, rtype, res):
+
+        if not (qname, rtype) in self.record_parser:
+            self.logger.warning("unable to find parser for %s %s" %
+                                (qname, rtype))
+            return {}
+
+        parser = self.record_parser[(qname, rtype)]
+
+        try:
+            qname_obj = dns.name.from_text(qname)
+            rtype_obj = dns.rdatatype.from_text(rtype)
+            parsed = parser(qname_obj, rtype_obj, res)
+            return parsed
+        except Exception as ex:
+            self.logger.warning("unexpected error %s occurred while parsing" %
+                                (str(ex)))
+            self.logger.warning("unable to parse %s %s with parser" %
+                                (qname, rtype))
+            return {}
+
+    def convert_influx_notation(self,
+                                current_time,
+                                time_diff,
+                                nameserver,
+                                dst,
+                                src,
+                                prb_id,
+                                af,
+                                proto,
+                                qname,
+                                rrtype,
+                                res):
+
+        measurement_name = self.cnfs.data_store.database
+
+        result = {"measurement": measurement_name,
+                  "time":        current_time,
+                  "tags": {
+                      "af": af,
+                      "dst_addr": dst,
+                      "dst_name": nameserver,
+                      "from": src,
+                      "prb_id": prb_id,
+                      "proto": proto,
+                      "rrtype": rrtype,
+                      "qname": qname},
+                  "fields": {}}
+
+        (err, con) = res
+
+        if err is not None:
+            result["fields"]["reason"] = str(err)
+            result["fields"]["time_took"] = time_diff
+            return result
+
+        parsed = self.parse_response_to_fields(qname, rrtype, con)
+        parsed["time_took"] = time_diff
+
+        result["fields"] = parsed
+
+        return result
+
+
+def soa_parser(qname_obj, rtype_obj, res):
+
+    rrset = res.get_rrset(res.answer, qname_obj, dns.rdataclass.IN, rtype_obj)
+
+    if (rrset is None) or (len(rrset) == 0):
+        return {}
+
+    record = rrset[0]
+    result = dict(id=res.id,
+                  ttl=rrset.ttl,
+                  name=str(rrset.name),
+                  mname=str(record.mname),
+                  rname=str(record.rname),
+                  serial=record.serial,
+                  type=dns.rdatatype.to_text(rtype_obj))
+
+    return result
+
+
+def ns_parser(qname_obj, rtype_obj, res):
+
+    rrset = res.get_rrset(res.answer, qname_obj, dns.rdataclass.IN, rtype_obj)
+
+    if (rrset is None) or (len(rrset) == 0):
+        return {}
+
+    # todo: 正しく実装！
+
+    return {}
+
+
+class Measurer(framework.BaseSetup):
 
     def __init__(self):
         super().__init__(__name__, __file__)
@@ -32,7 +141,9 @@ class Measurer(framework.Setup):
 # todo: 下行をコメントアウトを消す
 #        self.__validate_id()
         self.__load_measurement_info()
-        self.record_parser = {}
+        self.converter = MeasurementDataConverter(self.logger,
+                                                  self.cnfg,
+                                                  self.cnfs)
 
     def __set_measurer_id(self):
         hostname = socket.gethostname()
@@ -72,8 +183,6 @@ class Measurer(framework.Setup):
         self.ipv4 = selected_ipv4
         self.ipv6 = selected_ipv6
 
-        self.ipv4 = "10.0.2.15"
-
     def __validate_id(self):
         if self.ipv4 is None or self.ipv6 is None:
             self.logger.critical("measurer must have global IPv4/IPv6 address")
@@ -104,183 +213,184 @@ class Measurer(framework.Setup):
 
         try:
             json_obj = json.loads(response.text)
-            self.logger.info("json object load in properly")
+            self.logger.info("json object load in properly from controller")
         except json.decoder.JSONDecodeError as ex:
             self.logger.error("undecodable object responsed: %s" % (str(ex)))
             sys.exit(1)
 
         self.logger.debug(json_obj)
-        self.measurement_info = json_obj
 
-    def add_record_parser(self, qname, rtype, func):
-        self.record_parser[(qname, rtype)] = func
-
-    def parse_response_to_fields(self, qname, rtype, res):
-
-        if not (qname, rtype) in self.record_parser:
-            self.logger.warning("unable to find parser for %s %s" %
-                                (qname, rtype))
-            return {}
-
-        qname_obj = dns.name.from_text(qname)
-        rtype_obj = dns.rdatatype.from_text(rtype)
-
-        return self.record_parser[(qname, rtype)](qname_obj, rtype_obj, res)
-
-    def convert_influx_notation(self, current_time, nameserver, addr, af, proto, qname, rtype, res, err):
-        conf = self.conf_script["data_store"]
-        measurement_name = conf["database"]
-
-        result = {"measurement": measurement_name,
-                  "time":        current_time,
-                  "tags": {
-                      "af": af,
-                      "dst_addr": addr,
-                      "dst_name": nameserver,
-                      "from": self.ipv4 if af == 4 else self.ipv6,
-                      "msm_name": "Tdig",
-                      "prb_id": self.measurer_id,
-                      "proto": proto}}
-
-        if err is None:
-            result["fields"] = self.parse_response_to_fields(qname, rtype, res)
-        else:
-            result["fields"] = {"getaddrinfo": str(err)}
-
-        return result
-
-    def measure_toplevel(self):
-
-        conf = self.conf_script["measurement"]
-        tcp_timeout = float(conf["tcp_timeout"])
-        udp_timeout = float(conf["udp_timeout"])
-        current_time = str(datetime.datetime.utcnow().isoformat()) + "Z"
-        result = []
-
-        for measurement in self.measurement_info:
-            v4_dst = measurement["destination"]["v4"]
-            v6_dst = measurement["destination"]["v6"]
-            queries = measurement["query"]
-            nameserver = measurement["nameserver"]
-
-# todo: implement multithread
-            for addr in v4_dst:  # + v6_dst:
-                addr_obj = ipaddress.ip_address(addr)
-                source = self.ipv4 if addr_obj.version == 4 else self.ipv6
-                for query in queries:
-                    qname = query["qname"]
-                    rtype = query["rtype"]
-                    err_udp = res_udp = None
-                    err_tcp = res_tcp = None
-
-                    try:
-                        qo = dns.message.make_query(
-                            dns.name.from_text(qname),
-                            dns.rdatatype.from_text(rtype))
-                    except dns.rdatatype.UnknownRdatatype as ex:
-                        self.logger.warning("unknown query type: %s skipped measurement" %(rtype))
-                        continue
-
-                    try:
-                        res_udp = dns.query.udp(qo, str(addr_obj), timeout=udp_timeout, source=source)
-                    except dns.exception.Timeout as ex:
-                        self.logger.warning("timeout occurred while measurement: %s" %(ex))
-                        err_udp = ex
-                    except OSError as ex:
-                        self.logger.warning("os error occurred while measurement: %s" %(ex))
-                        err_udp = ex
-
-                    try:
-                        res_tcp = dns.query.tcp(qo, str(addr_obj), timeout=tcp_timeout, source=source)
-                    except dns.exception.Timeout as ex:
-                        err_tcp = ex
-                        self.logger.warning("timeout occurred while measurement: %s" %(ex))
-                    except OSError as ex:
-                        err_tcp = ex
-                        self.logger.warning("os error occurred while measurement: %s" %(ex))
-
-                    result.append(
-                        self.convert_influx_notation(
-                            current_time, nameserver, str(addr_obj), addr_obj.version, "udp", qname, rtype, res_udp, err_udp))
-                    result.append(
-                        self.convert_influx_notation(
-                            current_time, nameserver, str(addr_obj), addr_obj.version, "tcp", qname, rtype, res_tcp, err_tcp))
-
-        self.logger.info("%s data measured" % (len(result)))
-        return result
+        try:
+            self.measurement_info = namedtupled.map(json_obj)
+        except Exception:
+            self.logger.error("unable to map json obj to namedtuple")
+            sys.exit(1)
 
     def write_measurement_data(self, data):
-        conf = self.conf_script["data_store"]
-        host = conf["host"]
-        port = conf["port"]
-        user = conf["user"]
-        passwd = conf["passwd"]
-        database = conf["database"]
+        host = self.cnfs.data_store.host
+        port = self.cnfs.data_store.port
+        user = self.cnfs.data_store.user
+        passwd = self.cnfs.data_store.passwd
+        database = self.cnfs.data_store.database
 
-        client = influxdb.InfluxDBClient(host, int(port), user, passwd, database)
-
+        client = InfluxDBClient(host, int(port), user, passwd, database)
         ret = False
 
         try:
+            self.logger.info("writing data to influxdb")
             ret = client.write_points(data)
             if not ret:
-                self.logger.warning("writing measurement data to the influxdb seems to have failed")
-                self.logger.debug("while writing followeing data %s" %(data))
+                self.logger.warning("writing data to the influxdb failed")
+                self.logger.debug("while writing followeing %s" % str(data))
         except Exception as ex:
-            self.logger.warning("exception %s occurred while writing measurement data to influxdb" %(ex))
-            self.logger.debug("exception occurred while writing followeing data %s" %(data))
+            self.logger.warning("%s occurred while writing" % str(ex))
+            self.logger.debug("while writing following %s" % str(data))
         finally:
             client.close()
 
         return ret
 
-    def run(self):
+    def __measurement_core(self,
+                           current_time,
+                           nameserver,
+                           queryer,
+                           dst,
+                           src,
+                           af,
+                           timeout,
+                           proto,
+                           qname,
+                           rrtype,
+                           qo):
+
+        res = None
+        err = None
+
+        start_at = time.time()
+
         try:
-            result = self.measure_toplevel()
-            ret = self.write_measurement_data(result)
-            if not ret:
-                sys.exit(1)
+            res = queryer(qo, dst, timeout=timeout, source=src)
+        except dns.exception.Timeout as ex:
+            self.logger.warning("timeout while measurement: %s" % str(ex))
+            err = ex
+        except OSError as ex:
+            self.logger.warning("OSError while measurement: %s" % str(ex))
+            err = ex
         except Exception as ex:
-            self.logger.error("unexpected exception <%s> occurred" %(str(ex)))
-            self.logger.error(traceback.format_exc())
-            sys.exit(1)
+            self.logger.warning("unexpected error while measuremet")
+            self.logger.warning("%s" % str(ex))
+            err = ex
+        finally:
+            time_diff = time.time() - start_at
 
-        sys.exit(0)
+        writable = self.converter.convert_influx_notation(current_time,
+                                                          time_diff,
+                                                          nameserver,
+                                                          dst,
+                                                          src,
+                                                          self.measurer_id,
+                                                          af,
+                                                          proto,
+                                                          qname,
+                                                          rrtype,
+                                                          (err, res))
+        return writable
 
+    def measure_toplevel(self):
 
-def soa_parser(qname_obj, rtype_obj, res):
+        tcp_timeout = float(self.cnfs.measurement.tcp_timeout)
+        udp_timeout = float(self.cnfs.measurement.udp_timeout)
+        current_time = str(datetime.datetime.utcnow().isoformat()) + "Z"
+        result = []
 
-    result = {}
-    rrset = res.get_rrset(res.answer, qname_obj, dns.rdataclass.IN, rtype_obj)
+        queryer_info_by_protocol = {"udp": (dns.query.udp, udp_timeout),
+                                    "tcp": (dns.query.tcp, tcp_timeout)}
 
-    if (rrset is None) or (len(rrset) == 0):
-        self.logger.warning("unable to get RRset: qname=%s, rtype=%s, response=%s" %(qname, rtype, res))
+        threadpool = ThreadPool(processes=1)
+        async_results = []
+
+        for measurement in self.measurement_info:
+
+            nameserver = measurement.nameserver
+            protocol = measurement.proto
+            queries = measurement.query
+            v4_dst = measurement.destination.v4
+            v6_dst = measurement.destination.v6
+
+            # todo: コメントアウトを削除すること
+            for addr in v4_dst:  # + v6_dst:
+
+                addr_obj = ipaddress.ip_address(addr)
+
+                if addr_obj.version == 4:
+                    source = self.ipv4
+                elif addr_obj.version == 6:
+                    source = self.ipv6
+                else:
+                    self.logger.error("unknown version addr: %s" % str(addr))
+                    sys.exit(1)
+
+                for proto in protocol:
+
+                    queryer, timeout = queryer_info_by_protocol[proto]
+
+                    for query in queries:
+                        qname = query.qname
+                        rrtype = query.rrtype
+
+                        try:
+                            qname_obj = dns.name.from_text(qname)
+                            rrtype_obj = dns.rdatatype.from_text(rrtype)
+                            qo = dns.message.make_query(qname_obj, rrtype_obj)
+                        except dns.rdatatype.UnknownRdatatype:
+                            self.logger.warning("unknown query: %s" % (rrtype))
+                            self.logger.warning("measurement skipped")
+                            continue
+
+                        async_results.append(
+                            threadpool.apply_async(self.__measurement_core,
+                                                   (current_time,
+                                                    nameserver,
+                                                    queryer,
+                                                    addr,
+                                                    source,
+                                                    addr_obj.version,
+                                                    timeout,
+                                                    proto,
+                                                    qname,
+                                                    rrtype,
+                                                    qo
+                                                    )))
+        for each in async_results:
+            result.append(each.get())
+
+        self.logger.info("%s data measured" % (len(result)))
+        self.logger.debug("following is massured data %s" % str(result))
+
         return result
 
-    record = rrset[0]
-
-    result["id"] = res.id
-    result["rt"] = res.time
-    result["ttl"] = rrset.ttl
-    result["name"] = str(rrset.name)
-    result["mname"] = str(record.mname)
-    result["rname"] = str(record.rname)
-    result["serial"] = record.serial
-    result["type"] = dns.rdatatype.to_text(rtype_obj)
-
-    return result
+    def run(self):
+        result = self.measure_toplevel()
+        ret = self.write_measurement_data(result)
+        if not ret:
+            sys.exit(1)
+        sys.exit(0)
 
 
 if __name__ == "__main__":
 
     try:
         measurer = Measurer()
-        measurer.add_record_parser("jp", "SOA", soa_parser)
+        measurer.converter.add_record_parser("jp", "SOA", soa_parser)
+        measurer.converter.add_record_parser("jp", "NS", ns_parser)
+
+        # todo: 以下を削除すること
+        measurer.ipv4 = "10.0.2.15"
+
     except Exception:
         # LOGGERのセットアップ自体にも失敗している可能性ありの為
         # 標準出力にログ出力
         print(traceback.format_exc())
         sys.exit(1)
 
-    measurer.run()
-
+    measurer.start()
