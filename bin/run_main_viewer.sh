b0ef981755245b728e7c094473ccc85c76abe9a6
moratori <hoko1993@yahoo.co.jp>
2019-03-04 00:31:29 +0900

    可視化画面の起動スクリプトを作成

---
+++ bin/run_main_viewer.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#######################################
+CURRENT=$(cd $(dirname $0) && pwd)
+PROJECT_ROOT="${CURRENT%/}/.."
+BIN="${PROJECT_ROOT}/bin"
+
+. ${BIN}/common.sh
+
+SELF="`basename $0`"
+#######################################
+
+cd ${PROJECT_ROOT}
+
+
+## If exclusive control is required, please comment out the following
+#if ! ln -s $$ "${LOCKS}/${SELF}" > /dev/null 2>&1; then
+#    echo "the script ${SELF} seems to be running"
+#    echo "aborted"
+#    exit 1
+#fi
+
+
+pipenv run ${SOURCES}/main_viewer.py $@
+return_code=$?
+
+## If exclusive control is required, please comment out the following
+#rm "${LOCKS}/${SELF}"
+
+exit $return_code
+
+
