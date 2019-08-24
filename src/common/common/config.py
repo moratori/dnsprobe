325023d685cbb4710cedc1bad8e2332177d92cf3
moratori <hoko1993@yahoo.co.jp>
2019-08-25 00:34:18 +0900

    一時ファイルの保存先ディレクトリを設定

--- src/common/common/config.py
+++ src/common/common/config.py
@@ -12,6 +12,7 @@ if __name__ != "__main__":
         os.path.dirname(os.path.abspath(os.path.join(
             os.path.dirname(os.path.abspath(__file__)), "../../")))
 
+    TMP_DIR = os.path.join("/tmp", os.path.basename(PROJECT_ROOT))
     LOGS_DIR = os.path.join(PROJECT_ROOT, "logs")
     STATIC_DIR = os.path.join(PROJECT_ROOT, "static")
     TEMPLATES_DIR = os.path.join(PROJECT_ROOT, "templates")
