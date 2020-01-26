c89d70609d9f75a43499e366a338e0180baaa339
moratori <hoko1993@yahoo.co.jp>
2020-01-26 18:54:19 +0900

    セットアップに関するメソッドの単体テスト追加

--- test/test_main_measurer.py
+++ test/test_main_measurer.py
@@ -3,7 +3,12 @@
 import unittest
 import time
 import multiprocessing as mp
+import ipaddress
 import re
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import src.main_measurer_controller as controller
 import src.main_measurer as measurer
@@ -28,3 +33,17 @@ class TestMainMeasurer(unittest.TestCase):
         id_pattern = re.compile("^[a-z][a-z][a-z]\-[0-9]+$")
         self.measurer.set_measurer_id()
         self.assertTrue(id_pattern.findall(self.measurer.measurer_id))
+
+    def test_set_global_ipaddress(self):
+        self.measurer.set_global_ipaddress()
+        if self.measurer.ipv4 is not None:
+            self.assertTrue(ipaddress.ip_address(
+                self.measurer.ipv4).is_global)
+        else:
+            self.assertIsNone(self.measurer.ipv4)
+        if self.measurer.ipv6 is not None:
+            self.assertTrue(ipaddress.ip_address(
+                self.measurer.ipv6).is_global)
+        else:
+            self.assertIsNone(self.measurer.ipv6)
+
