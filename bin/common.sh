fd177fe2f22d40cd65b758dc4f22747d2d5f30ff
moratori <hoko1993@yahoo.co.jp>
2020-01-26 16:26:43 +0900

    venvファイルディレクトリ配下をテストから除外するためにパスを管理

--- bin/common.sh
+++ bin/common.sh
@@ -7,6 +7,7 @@ BIN="${PROJECT_ROOT}/bin"
 LOCKS="${PROJECT_ROOT}/jobs/locks"
 SOURCES="${PROJECT_ROOT}/src"
 TESTS="${PROJECT_ROOT}/test"
+VENV="${PROJECT_ROOT}/.venv"
 #######################################
 
 # do for common setting
