33f8da5a20322c71be8adc29a51632fd3b52fb93
moratori <hoko1993@yahoo.co.jp>
2020-02-02 12:56:22 +0900

    単体テストでは、controllerとの接続は確認しないので除外

--- test/test_main_measurer.py
+++ test/test_main_measurer.py
@@ -1,40 +1,33 @@
 #!/usr/bin/env python3
 
 import unittest
-import time
-import multiprocessing as mp
 import ipaddress
 import re
 import sys
 import os
+import time
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
 
 import main_measurer_controller as controller
 import main_measurer as measurer
+import common.data.errors as errors
 
 
 class TestMainMeasurer(unittest.TestCase):
 
     def setUp(self):
-
-        self.controller = mp.Process(target=controller.nakedserver,
-                                     args=())
-        self.controller.start()
-
-        time.sleep(4)
-
         self.measurer = measurer.Measurer()
 
     def tearDown(self):
-        self.controller.terminate()
+        pass
 
-    def test_set_measurer_id(self):
+    def test_0_set_measurer_id(self):
         id_pattern = re.compile("^[a-z][a-z][a-z]\-[0-9]+$")
         self.measurer.set_measurer_id()
         self.assertTrue(id_pattern.findall(self.measurer.measurer_id))
 
-    def test_set_global_ipaddress(self):
+    def test_1_set_global_ipaddress(self):
         self.measurer.set_global_ipaddress()
         if self.measurer.ipv4 is not None:
             self.assertTrue(ipaddress.ip_address(
@@ -47,3 +40,13 @@ class TestMainMeasurer(unittest.TestCase):
         else:
             self.assertIsNone(self.measurer.ipv6)
 
+    def test_2_set_server_boottime(self):
+        self.measurer.set_server_boottime()
+        pat = re.compile("^[0-9]{4}\-[0-9]{2}\-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{6}Z$")
+        self.assertTrue(pat.findall(self.measurer.server_boottime))
+
+    def test_3_validate_id(self):
+        self.measurer.set_global_ipaddress()
+        if self.measurer.ipv4 is None or self.measurer.ipv6 is None:
+            self.assertRaises(errors.DNSProbeError,
+                              self.measurer.validate_id)
