03c01a72dd3bd8eb71369ca35d6787b1fd447d55
moratori <hoko1993@yahoo.co.jp>
2020-02-08 23:10:32 +0900

    テストケース追加

--- test/test_unit_main_measurer.py
+++ test/test_unit_main_measurer.py
@@ -5,8 +5,11 @@ import ipaddress
 import re
 import sys
 import os
+import datetime
 import multiprocessing as mp
 import time
+import dns.query
+import namedtupled
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
 
@@ -73,3 +76,158 @@ class TestMainMeasurer(unittest.TestCase):
         self.assertTrue(len(self.measurer.measurement_info) >= 0)
 
         proc.terminate()
+
+    def test_6_load_measurement_info(self):
+        self.assertRaises(errors.DNSProbeError, 
+                          self.measurer.load_measurement_info)
+        self.assertFalse(hasattr(self.measurer, "measurement_info"))
+
+    def test_7_measurement_core(self):
+
+        self.measurer.set_measurer_id()
+        self.measurer.set_server_boottime()
+
+        current_time = str(datetime.datetime.utcnow().isoformat()) + "Z"
+        nameserver = "a.dns.jp"
+        queryer = dns.query.udp
+        addr = "203.119.1.1"
+        source = "10.0.2.15"  # rewrite addr depending on test environment
+        version = 4
+        asn = "ASXXXX"
+        asn_desc = "test AS"
+        timeout = 2.5
+        protocol = "udp"
+        qname = "jp"
+        rrtype = "SOA"
+
+        qo = dns.message.make_query(dns.name.from_text(qname),
+                                    dns.rdatatype.from_text(rrtype),
+                                    use_edns=True)
+        qo.flags &= 0xFEFF
+        qo.use_edns(edns=0,
+                    options=[dns.edns.GenericOption(dns.edns.NSID,
+                                                    bytes())])
+        self.assertTrue(self.measurer.measurement_core(
+            current_time,
+            nameserver,
+            queryer,
+            addr,
+            source,
+            version,
+            asn,
+            asn_desc,
+            timeout,
+            protocol,
+            qname,
+            rrtype,
+            qo))
+
+    def test_8_measurement_core(self):
+
+        self.measurer.set_measurer_id()
+        self.measurer.set_server_boottime()
+
+        current_time = str(datetime.datetime.utcnow().isoformat()) + "Z"
+        nameserver = "a.dns.jp"
+        queryer = dns.query.tcp
+        addr = "203.119.1.1"
+        source = "10.0.2.15"  # rewrite addr depending on test environment
+        version = 4
+        asn = "ASXXXX"
+        asn_desc = "test AS"
+        timeout = 2.5
+        protocol = "tcp"
+        qname = "jp"
+        rrtype = "SOA"
+
+        qo = dns.message.make_query(dns.name.from_text(qname),
+                                    dns.rdatatype.from_text(rrtype),
+                                    use_edns=True)
+        qo.flags &= 0xFEFF
+        qo.use_edns(edns=0,
+                    options=[dns.edns.GenericOption(dns.edns.NSID,
+                                                    bytes())])
+        self.assertTrue(self.measurer.measurement_core(
+            current_time,
+            nameserver,
+            queryer,
+            addr,
+            source,
+            version,
+            asn,
+            asn_desc,
+            timeout,
+            protocol,
+            qname,
+            rrtype,
+            qo))
+
+    def test_9_measurement_core(self):
+
+        self.measurer.set_measurer_id()
+        self.measurer.set_server_boottime()
+
+        current_time = str(datetime.datetime.utcnow().isoformat()) + "Z"
+        nameserver = "a.dns.jp"
+        queryer = dns.query.tcp
+        addr = "203.119.1.1"
+        source = "10.0.2.15"  # rewrite addr depending on test environment
+        version = 4
+        asn = "ASXXXX"
+        asn_desc = "test AS"
+        timeout = 0
+        protocol = "tcp"
+        qname = "jp"
+        rrtype = "SOA"
+
+        qo = dns.message.make_query(dns.name.from_text(qname),
+                                    dns.rdatatype.from_text(rrtype),
+                                    use_edns=True)
+        qo.flags &= 0xFEFF
+        qo.use_edns(edns=0,
+                    options=[dns.edns.GenericOption(dns.edns.NSID,
+                                                    bytes())])
+        self.assertTrue(self.measurer.measurement_core(
+            current_time,
+            nameserver,
+            queryer,
+            addr,
+            source,
+            version,
+            asn,
+            asn_desc,
+            timeout,
+            protocol,
+            qname,
+            rrtype,
+            qo))
+
+    def test_10_measurement_toplevel(self):
+        raw_measurement_info = [
+            {"nameserver": "a.dns.jp",
+             "destination": [
+                 "203.119.1.1"],
+             "proto": "udp",
+             "query": {
+                 "qname": "jp",
+                 "rrtype": "SOA"
+             }
+            },
+            {"nameserver": "a.dns.jp",
+             "destination": [
+                 "203.119.1.1"
+             ],
+             "proto": "tcp",
+             "query": {
+                 "qname": "jp",
+                 "rrtype": "SOA"
+             }}]
+
+        self.measurer.measurement_info = namedtupled.map(raw_measurement_info)
+        self.measurer.set_measurer_id()
+        self.measurer.set_server_boottime()
+        self.measurer.ipv4 = "10.0.2.15"  # rewrite addr depending on test environment
+        self.measurer.net_desc_v4 = ("", "")
+
+        self.assertTrue(
+            self.measurer.measure_toplevel())
