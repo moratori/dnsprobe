00c41475b6dff48d69b583ba5e69a2388da38ff4
moratori <hoko1993@yahoo.co.jp>
2020-05-04 22:27:02 +0900

    テストデータ生成用スクリプトコミット

---
+++ test/data/make_data_performance.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""
+InfluxDBのline protocolで、テスト用データを作成する
+生成したテストデータは、標準出力に出力する
+"""
+
+import datetime
+import argparse
+import itertools
+
+
+TRANSPORT = ["tcp", "udp"]
+AF = [4, 6]
+RRTYPE = ["SOA"]
+
+
+def generate(argument):
+
+    end_time = datetime.datetime.now()
+    start_time = \
+        end_time - datetime.timedelta(minutes=argument.range_in_minutes)
+    probes = ["prb%d" % n for n in range(argument.probes)]
+    nameservers = ["srv%d" % n for n in range(argument.nameservers)]
+
+    while (start_time < end_time):
+        for (nameserver, probe, af, rrtype) in itertools.product(nameservers,
+                                                                 probes,
+                                                                 AF,
+                                                                 RRTYPE):
+            pass
+        start_time += datetime.timedelta(minutes=argument.step_in_minutes)
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--nameservers", type=int, default=8)
+    argparser.add_argument("--probes", type=int, default=20)
+    argparser.add_argument("--range-in-minutes", type=int, default=3)
+    argparser.add_argument("--step-in-minutes", type=int, default=1)
+
+    generate(argparser.parse_args())
